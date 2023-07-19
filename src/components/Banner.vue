21da855e28e656fbeebea1fad91249be092dd7de
2977094657 <2977094657@qq.com>
2023-07-19 22:56:05 +0800

    完成Banner右边的部分显示信息，登录窗口已完成

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -2,7 +2,28 @@
 import { ref, onMounted } from 'vue'
 import router from "@/router/router";
 import { HeartOutlined } from '@ant-design/icons-vue';
-import {Document} from "@element-plus/icons-vue";
+import {ChatDotSquare, Document, ShoppingCart} from "@element-plus/icons-vue";
+import Login from "@/components/Login.vue";
+
+let isCartHovered = ref(false);
+
+const handleCartMouseOver = () => {
+  isCartHovered.value = true;
+};
+
+const handleCartMouseOut = () => {
+  isCartHovered.value = false;
+};
+
+let isReviewHovered = ref(false);
+
+const handleReviewMouseOver = () => {
+  isReviewHovered.value = true;
+};
+
+const handleReviewMouseOut = () => {
+  isReviewHovered.value = false;
+};
 
 let isOrderHovered = ref(false);
 
@@ -38,10 +59,25 @@ const goToProduct = (productId) => {
   const url = router.resolve({ name: 'Product', params: { productId } }).href;
   window.open(url, '_blank');
 }
+
+let open = ref(false)
+
+const showModal = () => {
+  open.value = true
+}
+
+const handleOk = () => {
+  open.value = false
+}
+
 onMounted(banner)
 </script>
 
 <template>
+    <a-modal width="820px" :footer="null" :maskClosable="false" v-model:visible="open" @ok="handleOk">
+      <Login></Login>
+    </a-modal>
+
   <div class="banner">
     <el-carousel indicator-position="outside">
       <el-carousel-item v-for="Banner in Banner" :key="Banner.productId">
@@ -51,12 +87,10 @@ onMounted(banner)
   </div>
 
   <div class="head">
-    <img class="img" src="http://1.14.126.98:5000/R.jpg" alt="头像">
+    <img class="img" src="http://1.14.126.98:5000/R.jpg" alt="头像" @click="showModal">
     <p>Hi!&nbsp;你好</p>
     <button class="bt1">登录</button>
     <button class="bt2">注册</button><br><br>
-    <div class="my">
-
       <div @mouseover="handleMouseOver" @mouseout="handleMouseOut" class="clickable">
         <div class="heart1">
           <HeartOutlined :style="{ fontSize: '20px' }" :class="{ 'highlight': isHovered }"/>
@@ -75,103 +109,27 @@ onMounted(banner)
           我的订单
         </div>
       </div>
-    </div>
+
+      <div class="clickable" @mouseover="handleReviewMouseOver" @mouseout="handleReviewMouseOut">
+        <div class="ChatDotSquare">
+          <el-icon style="font-size: 20px;" :class="{ 'highlight': isReviewHovered }"><ChatDotSquare /></el-icon>
+        </div>
+        <div class="pj" :class="{ 'highlight': isReviewHovered }">
+          我的评价
+        </div>
+      </div>
+
+      <div class="clickable" @mouseover="handleCartMouseOver" @mouseout="handleCartMouseOut">
+        <div class="ShoppingCart">
+          <el-icon style="font-size: 20px;" :class="{ 'highlight': isCartHovered }"><ShoppingCart /></el-icon>
+        </div>
+        <div class="Shopping" :class="{ 'highlight': isCartHovered }">
+          购物车
+        </div>
+      </div>
   </div>
 </template>
 
 <style scoped>
-.highlight {
-  color: #F40;
-}
-
-.banner {
-  overflow: hidden;
-  border-radius: 10px;
-  margin: 10px 0 0 23%;
-  width: 48%;
-}
-
-img {
-  border-radius: 10px;
-  width: 100%;
-  height: 100%;
-  opacity: 1;
-  transition: opacity 0.3s;
-}
-
-.head{
-  top: 200px;
-  right: 11%;
-  position: absolute;
-  width: 21%;
-  text-align: center;
-}
-
-.img{
-  border: 1px #FF5000 solid;
-  border-radius: 100px;
-  height: 70px;
-  width: 70px;
-}
-
-button{
-  border-radius: 21px;
-  font-size: 16px;
-  width: 80px;
-  height: 42px;
-  border: none;
-  color: white;
-}
-
-.bt1{
-  font-size: 16px;
-  font-weight: 700;
-  line-height: 1.5;
-  font-family: tahoma, arial, 'Hiragino Sans GB', '\5b8b\4f53', sans-serif;
-  background-image: linear-gradient(to right,#ff5000 0,#ff6000 100%);
-}
-
-.bt2{
-  font-size: 16px;
-  font-weight: 700;
-  line-height: 1.5;
-  font-family: tahoma, arial, 'Hiragino Sans GB', '\5b8b\4f53', sans-serif;
-  background-image: linear-gradient(to right,#ff9000 0,#ff7000 100%);
-  margin: 0 0 0 10px;
-}
-
-.my{
-  color: #3c3c3c;
-}
-
-.heart-outlined{
-  position: absolute;
-  width: 65px;
-  top: 250px;
-  left: 10px;
-  font: 12px/1.5 tahoma,arial,'Hiragino Sans GB','\5b8b\4f53',sans-serif;
-}
-
-.heart1{
-  position: absolute;
-  top: 220px;
-  left: 30px
-}
-
-.Document{
-  position: absolute;
-  left: 30%;
-}
-
-.dd{
-  position: absolute;
-  top: 250px;
-  left: 90px;
-  width: 65px;
-  font: 12px/1.5 tahoma,arial,'Hiragino Sans GB','\5b8b\4f53',sans-serif;
-}
-
-.clickable {
-  cursor: pointer;
-}
+@import '../assets/Banner.css';
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,72 @@
+<script setup>
+import { ref, computed } from 'vue';
+
+let currentForm = ref('login');
+let phoneNumber = ref('');
+let password = ref('');
+let phoneNumberTouched = ref(false);
+
+const switchForm = form => {
+  currentForm.value = form;
+}
+
+const phoneNumberIsValid = computed(() => /^1[3-9]\d{9}$/.test(phoneNumber.value));
+
+const handleBlur = () => {
+  phoneNumberTouched.value = true;
+}
+
+const handleFocus = () => {
+  phoneNumberTouched.value = false;
+}
+
+const login = () => {
+  if (!phoneNumberIsValid.value) {
+    alert('请输入有效的手机号码');
+
+  }
+
+  // 在这里添加登录逻辑
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="logo">
+      <img src="" alt="Logo">
+    </div>
+    <div class="vertical-line"></div>
+    <div class="form-container">
+      <div class="wz">
+        <span @click="switchForm('login')" :class="{ active: currentForm === 'login' }" class="form-switcher">登录</span>
+        <div class="line"></div>
+        <span @click="switchForm('register')" :class="{ active: currentForm === 'register' }" class="form-switcher">注册</span>
+      </div>
+
+      <form v-if="currentForm === 'login'" @submit.prevent="login" class="login">
+        <div class="input-phone">
+          <div class="sjh">
+            手机号
+          </div>
+          <input placeholder="请输入手机号" class="phone" id="phone-number" type="tel" v-model="phoneNumber" @blur="handleBlur" @focus="handleFocus" required/>
+            <hr style="margin-top: 45px;border: none;height: 0.5px;background-color: #e3e5e7;">
+          <div style="left: 37px;top: 57px;position: absolute">
+            密码
+          </div>
+          <div class="input-group">
+          <input placeholder="请输入密码" class="password" id="password" type="password" v-model="password" required/>
+        </div>
+        </div>
+        <button class="dl">登录</button>
+      </form>
+
+    <form v-else-if="currentForm === 'register'">
+      <!-- 注册表单的内容 -->222
+    </form>
+  </div>
+  </div>
+</template>
+
+<style scoped>
+@import '../assets/Login.css';
+</style>
